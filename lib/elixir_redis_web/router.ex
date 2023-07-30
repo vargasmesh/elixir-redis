b53851fdf80bafc09dc323f0b2f87bf3845ef627
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-07-30 10:18:38 -0300

    feat: add cache

--- lib/elixir_redis_web/router.ex
+++ lib/elixir_redis_web/router.ex
@@ -1,6 +1,7 @@
 defmodule ElixirRedisWeb.Router do
   use Plug.Router
   alias ElixirRedis.TransactionService
+  alias ElixirRedis.Redix
 
   plug Plug.Parsers,
     parsers: [:json],
@@ -11,10 +12,21 @@ defmodule ElixirRedisWeb.Router do
   plug :dispatch
 
   get "/total-amount/:hash" do
+    response =
+      case Redix.command(["JSON.GET", hash]) do
+        {:ok, nil} ->
+          result = Jason.encode!(TransactionService.calculate_total_received_amount(hash))
+          Redix.command(["JSON.SET", hash, "$", result])
+          result
+
+        {:ok, value} ->
+          value
+      end
+
     send_resp(
       conn,
       200,
-      Jason.encode!(TransactionService.calculate_total_received_amount(hash))
+      response
     )
   end
 
