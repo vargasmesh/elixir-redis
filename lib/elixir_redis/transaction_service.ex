44d73064fe7bdc54aeb1775fccae0ac1988ac5f2
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-07-30 09:46:32 -0300

    feat: calculate total received amount

---
+++ lib/elixir_redis/transaction_service.ex
@@ -0,0 +1,16 @@
+defmodule ElixirRedis.TransactionService do
+  import Ecto.Query
+  import DateTime
+
+  alias ElixirRedis.Transaction
+  alias ElixirRedis.Repo
+
+  def calculate_total_received_amount(to_hash) do
+    query =
+      from t in Transaction,
+        where: t.to_hash == ^to_hash,
+        select: %{total: sum(t.amount)}
+
+    Map.put(Repo.one(query), :calculated_at, DateTime.utc_now())
+  end
+end
